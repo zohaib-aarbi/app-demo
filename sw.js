c392b2a361b89ef2a2decaebac66f66c56726b26
zohaib-aarbi <zohaibarbi05@gmail.com>
2026-05-06 08:04:09 +0300

    all file modifies

---
+++ sw.js
@@ -0,0 +1,9 @@
+javascript
+self.addEventListener("install", function (event) {
+    console.log("Service Worker Installed");
+});
+
+self.addEventListener("fetch", function (event) {
+    // basic service worker placeholder
+});
+
